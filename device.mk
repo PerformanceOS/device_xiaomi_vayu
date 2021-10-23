04f62de3267178f86dbf65555a448aadd25a82c7
johnmart19 <johnivan19999@gmail.com>
2021-10-23 04:58:12 +0300

    vayu: Add building of Neural Networks

--- device.mk
+++ device.mk
@@ -354,6 +354,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3 \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # Overlays
 PRODUCT_PACKAGES += \
    CarrierConfigOverlayVayu \
