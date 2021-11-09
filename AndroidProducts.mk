a23041f2f08203e12ca4e683bb87471e8d066c1b
johnmart19 <johnivan19999@gmail.com>
2021-11-09 21:24:03 +0200

    vayu: Welcome to FluidOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2018-2021 FluidOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/fluid_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    fluid_vayu-eng \
+    fluid_vayu-userdebug \
+    fluid_vayu-user
--- fluid_vayu.mk
+++ fluid_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2018-2021 FluidOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,14 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common fluidOS configurations
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := fluid_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+WITH_GMS := true
+
+# GApps
+ifeq ($(WITH_GMS),true)
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+endif
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
